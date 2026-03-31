fbd912338f4b358b3b637810dfb058cb2500c4d3
FarizIbrahimov19866 <fibrahimov19866@ada.edu.az>
2026-03-31 13:32:22 +0400

    Just a test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Further made changes to the output so the dictatorship is happy with the results.");
+        System.out.println("Further made changes to the output so the dictatorship is happy with the results. ");
     }
 }
 
